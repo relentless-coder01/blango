facc931349bb177ebdda673f0be347f4eda1d791
Deepika Sivakumar <deepikasivakumar@logisticsalliance.com>
2024-01-05 16:34:57 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -45,8 +45,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'blog',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
