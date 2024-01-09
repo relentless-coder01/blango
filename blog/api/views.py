1312d5bbce1559b157cdaaa51d98da6643863f30
Deepika Sivakumar <deepikasivakumar@logisticsalliance.com>
2024-01-09 19:22:01 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
-
+from django.contrib.auth.models import User
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -13,3 +13,8 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field="email"
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
