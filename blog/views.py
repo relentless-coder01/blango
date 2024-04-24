164aebae58b406e607db6c5223e13e3639d27ac6
Deepika Sivakumar <deepikasivakumar@logisticsalliance.com>
2024-04-24 23:02:50 +0000

    Finish logging.

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,19 @@ from blog.models import Post
 
 from django.shortcuts import render, get_object_or_404
 
+import logging
+logger = logging.getLogger(__name__)
+
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
   post = get_object_or_404(Post, slug=slug)
+  logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+  )
   if request.user.is_active:
     if request.method == "POST":
       comment_form = CommentForm(request.POST)
