2ed752f2eec51b231480bb004f1173c955cbfcf6
Deepika Sivakumar <deepikasivakumar@logisticsalliance.com>
2024-05-07 18:33:01 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from blog.models import Post
 
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 import logging
 logger = logging.getLogger(__name__)
@@ -47,4 +48,6 @@ def get_ip(request):
 
 
 def post_table(request):
-  return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
